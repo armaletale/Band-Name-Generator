5844e6b656c77e4485f4ddfa6e8cbceb4e434f91
youngdiggy <abdallahafigun2000@gmail.com>
2020-12-09 22:39:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
+
+print("Hello, Welcome  to Ginga Name Generator ")
+
+name_city = input("What city did you grow up? \n")
+
+name_pet =input("Name any pet \n")
+
+print(name_city +" "+ name_pet)
+
